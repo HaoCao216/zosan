5def00ae9194f5c97420a852a8c5928e4713d284
Vy Quốc Vũ <vyquocvu@gmail.com>
2019-10-05 16:41:22 +0700

    env now

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,16 +2,7 @@ import firebase from 'firebase';
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const prodConfig = {
-  apiKey: process.env.REACT_APP_PROD_API_KEY,
-  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-};
-
-const devConfig = {
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -20,7 +11,6 @@ const devConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
-const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 class Firebase {
   constructor() {
